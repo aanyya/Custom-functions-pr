efc9a49d6b3118843eb71659488cbb9049e60f5c
aanyya <annatselykovskaya@gmail.com>
2023-11-07 12:00:49 +0300

    JS

--- DOM/задания Dom.js
+++ DOM/задания Dom.js
@@ -1,1 +1,15 @@
-hghgh
+let nums = document.querySelectorAll('ul li');
+for (let num of nums) {
+	num.addEventListener('click', function func() {
+        if (this.textContent < 10){
+            this.textContent = Number(this.textContent) + 1;
+            this.removeEventListener('click', func)
+        } else {
+            alert(this.textContent + ' ' + '> 10')
+        }
+    });
+}
+
+
+
+
